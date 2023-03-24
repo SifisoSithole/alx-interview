ec5563cd248df1c24f3b31158718fb59a6b08fbd
SifisoSithole <sitholesifisobrian@gmail.com>
2023-03-24 13:16:07 +0200

    solution to pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,24 @@
+def pascal_triangle(n):
+    """
+    returns a list of lists of integers representing
+    the Pascal’s triangle of n
+    """
+
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    triangleIndex = 0
+    for i in range(1, n):
+        newLevel = [1]
+        j = 0
+        leng = len(triangle[triangleIndex])
+        while j + 1 != leng:
+            num = triangle[triangleIndex][j] + triangle[triangleIndex][j + 1]
+            newLevel.append(num)
+            j += 1
+        newLevel.append(1)
+        triangle.append(newLevel)
+        triangleIndex += 1
+
+    return triangle
