80388e062ef7c273438b721d3168cdfe4ac589e2
SifisoSithole <103381999+SifisoSithole@users.noreply.github.com>
2023-06-21 09:07:31 +0000

    solution

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,54 +1,31 @@
 #!/usr/bin/python3
-"""
-Prime game
-"""
+""" Module for Prime Game """
 
 
 def isWinner(x, nums):
-    """
-    winner of Prime game
-    """
-    def is_prime(num):
-        """
-        check if prime
-        """
-        if num < 2:
-            return False
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-    def play_game(n):
-        """ play game """
-        if n == 1:
-            return "Ben"
-
-        numbers = set(range(2, n + 1))
-        player = "Maria"
-
-        while True:
-            prime = None
-            for num in numbers:
-                if is_prime(num):
-                    prime = num
-                    break
-
-            if prime is None:
-                return player
-
-            numbers.difference_update(range(prime, n + 1, prime))
-
-            player = "Ben" if player == "Maria" else "Maria"
-
-    winners = [play_game(n) for n in nums]
-
-    maria_wins = winners.count("Maria")
-    ben_wins = winners.count("Ben")
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif maria_wins < ben_wins:
-        return "Ben"
-    else:
+    """Solves Prime Game"""
+    if not nums or x < 1:
         return None
+    n = max(nums)
+    sieve = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not sieve[i]:
+            continue
+        for j in range(i*i, n + 1, i):
+            sieve[j] = False
+
+    sieve[0] = sieve[1] = False
+    c = 0
+    for i in range(len(sieve)):
+        if sieve[i]:
+            c += 1
+        sieve[i] = c
+
+    player1 = 0
+    for n in nums:
+        player1 += sieve[n] % 2 == 1
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
